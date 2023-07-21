25eb4640171bfe963db317346bf6d85b2b937914
Chaerul Umam <chaerulumamb244@gmail.com>
2023-07-21 21:07:49 +0700

    protected user, wallet, tip model

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,10 +17,15 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    protected $table = 'users';
     protected $fillable = [
         'name',
         'email',
         'password',
+        'username',
+        'profile_picture',
+        'ktp',
+        'verified'
     ];
 
     /**
